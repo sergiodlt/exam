2abec263d71b39bbfc6a5b47fe4041c1ea44c457
sergio <sergio@examen.com>
2023-03-08 19:38:50 +0000

    v2

--- nombrejava29.java
+++ nombrejava29.java
@@ -1,5 +1,5 @@
 class Minombrejava29 {
 	public static void main(String[] args){
-		System.out.println("Mi nombre es Sergio y he creado el servidor ubunserver29");
+		System.out.println("Mi nombre es Sergio y he creado el servidor ubunserver29 y vivo en mejorada del campo");
 }
 }
